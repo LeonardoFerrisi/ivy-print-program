ae105da4fa9212a2ef4107c50f84162c8fd04c5e
LeonardoFerrisi <86019814+LeonardoFerrisi@users.noreply.github.com>
2026-06-02 18:00:05 -0600

    initial commit non working on windows

---
+++ ivy2/utils.py
@@ -0,0 +1,24 @@
+def parse_bit_range(input, size):
+    str = ""
+
+    for i in range(size):
+        str += "1" if ((input >> i) & 1) == 1 else "0"
+
+    try:
+        return int(str[::-1], 2)
+    except:
+        return 0
+
+
+def parse_incoming_message(data):
+    """General parser for all messages coming from the printer."""
+
+    # Ignore empty/partial frames; callers can continue waiting for a full message.
+    if not data or len(data) < 8:
+        return None
+
+    payload = data[8:len(data)]
+    ack = (data[6] & 255) | ((data[5] & 255) << 8)
+    error = data[7] & 255
+
+    return data, payload, ack, error
